03f7886902a206622d627328f9dd11e377e78790
fastgamer917 <34210142+fastgamer917@users.noreply.github.com>
2020-05-02 13:21:30 +0530

    Update reusable.cs

--- reusable.cs
+++ reusable.cs
@@ -29,3 +29,34 @@ Invode permission for cross-thread:-
             var pageUrl = siteurl;
            //all other code;
         }
+
+Accessing WinForms on another threads:-
+   Class Form1:-
+   private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 cls = new Class1(this);
+
+
+           // Task.Run(()=>Class1.runcode());
+        }
+Class Class1:-
+   public  Class1(Form1 form) //constructor receiving current instance of form.
+        {
+            Task.Run(() => runcode(form));
+        }
+        public static async void runcode(Form1 form)
+        {
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync("https://www.google.com");
+            Console.WriteLine(response);
+            if(form.textBox1.InvokeRequired)
+            {
+                form.textBox1.Invoke(new MethodInvoker(delegate { form.textBox1.Text = response; }));
+            }
+            else
+            form.textBox1.Text = "hello working";
+            
+           
+        }
+
+creating new instance of Form1 and the updating textbox wont help because completely new instace is created and wont update existing textbox.
